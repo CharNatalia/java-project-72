6cc12c4ecce1cdc78a158c94be0374cd5a5a20e1
Natalia Goncharenko <muratovan0921@gmail.com>
2026-01-11 17:29:42 +0300

    new project

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.github.ben-manes.versions") version "0.51.0"
+    application
+    id("java")
+    id("org.sonarqube") version "7.2.2.6593"
+    checkstyle
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+sonar {
+    properties {
+        property("sonar.projectKey", "CharNatalia_java-project-72")
+        property("sonar.organization", "charnatalia")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
