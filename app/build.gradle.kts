f9c24c8a3c0c8b8e8d7cf76b6c87d88c72c2ea86
Natalia Goncharenko <muratovan0921@gmail.com>
2026-01-13 16:07:17 +0300

    add javalin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,7 @@
+
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     id("com.github.ben-manes.versions") version "0.51.0"
     application
@@ -29,10 +33,21 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4.2")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.0")
     implementation ("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.16.2")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("gg.jte:jte:3.1.9")
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
 
 checkstyle {
